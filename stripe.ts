a7575e7e4ad4a1926c634892e94d8fe5e2dfc1d5
Otito Amuga <urphasin@gmail.com>
2026-05-26 22:30:08 -0600

    push

--- stripe.ts
+++ stripe.ts
@@ -1,15 +1,29 @@
 import "dotenv/config";
+import express from "express";
+import cors from "cors";
 import { log } from "console";
 import Stripe from "stripe";
+
+
+const app = express();
+const PORT = 5432;
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 // console.log(stripe);
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
+
+
+/**
+ * ROUTE: Create PaymentIntent
+ */
 const paymentIntent = await stripe.paymentIntents.create({
     amount: 100,
     currency: "usd"
 });
 
-log(paymentIntent);
+log(paymentIntent.client_secret);
